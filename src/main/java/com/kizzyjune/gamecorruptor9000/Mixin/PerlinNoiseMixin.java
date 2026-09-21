9989802a99a692f3daa76531ebf43a1af75a1583
kizzy-june <kizzy-june@proton.me>
2026-09-21 02:59:50 +0200

    Remove wrap() precision and multiply by four

--- src/main/java/com/kizzyjune/gamecorruptor9000/Mixin/PerlinNoiseMixin.java
+++ src/main/java/com/kizzyjune/gamecorruptor9000/Mixin/PerlinNoiseMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PerlinNoise.class)
 public class PerlinNoiseMixin {
    @Inject(method = "wrap",at = @At("RETURN"),cancellable = true)
-    private static void removePrecision(double in, CallbackInfoReturnable<Double> cir) {
+    private static void removePrecisionAndMultiplyByFour(double in, CallbackInfoReturnable<Double> cir) {
        double in2 = (int) in;
        cir.setReturnValue(in2 * 4);
    }
